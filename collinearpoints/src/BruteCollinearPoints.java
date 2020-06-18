16f43bfcaac4efcda4ea7a7f710e354aad259045
gotgotaind <gotgotaind@gmail.com>
2020-06-18 23:34:16 +0200

    Testing BruteCollinearPoints

--- collinearpoints/src/BruteCollinearPoints.java
+++ collinearpoints/src/BruteCollinearPoints.java
@@ -1,6 +1,7 @@
+import edu.princeton.cs.algs4.*;
+
 public class BruteCollinearPoints {
-    static LineSegment[] segments;
-    static int num_segemnts=0;
+    static Queue<LineSegment> segmentsQ=new Queue();
 
     public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
     {
@@ -13,9 +14,11 @@ public class BruteCollinearPoints {
             if( points[i] == null ) {
                 throw new IllegalArgumentException("points["+i+"] is null");
             }
-            for(int j=1;j<points.length;j++) {
-                if (points[i].equals(points[j])) {
-                    throw new IllegalArgumentException("points[" + i + "] is equal to points[" + j + "]");
+            for(int j=0;j<points.length;j++) {
+                if ( i != j ) {
+                    if (points[i].equals(points[j])) {
+                        throw new IllegalArgumentException("points[" + i + "] is equal to points[" + j + "]");
+                    }
                 }
             }
         }
@@ -30,7 +33,7 @@ public class BruteCollinearPoints {
                         Point s = points[l];
                         if( s.compareTo(r) > 0 && r.compareTo(q) > 0 && q.compareTo(p) > 0 ) {
                             if (p.slopeTo(q) == p.slopeTo(r) && p.slopeTo(q) == p.slopeTo(s)) {
-
+                                segmentsQ.enqueue(new LineSegment(p,s));
                             }
                         }
                     }
@@ -39,5 +42,51 @@ public class BruteCollinearPoints {
         }
     }
     public           int numberOfSegments()        // the number of line segments
+    {
+        return segmentsQ.size();
+    }
     public LineSegment[] segments()                // the line segments
+    {
+        LineSegment[] lsa=new LineSegment[segmentsQ.size()];
+        int i=0;
+        for ( LineSegment ls : segmentsQ ) {
+            lsa[i]=ls;
+            i=i+1;
+        }
+        return lsa;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        StdOut.println("Showing segments :");
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdOut.println("StdDraw.show();");
+        StdDraw.show();
+        StdOut.println("ZOBI");
+    }
+
 }
