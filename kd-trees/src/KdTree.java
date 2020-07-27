0230d14cd2f38e5bc40c9452099283293acf7c29
gotgotaind <gotgotaind@gmail.com>
2020-07-27 23:53:07 +0200

    insert and contains implemented. Tests are not conclusive.

--- kd-trees/src/KdTree.java
+++ kd-trees/src/KdTree.java
@@ -2,25 +2,22 @@ import edu.princeton.cs.algs4.*;
 
 public class KdTree {
     private Node root;
+    private int size;
 
     class Node {
         Point2D p;
         Node ld;
         Node ru;
-        int N; // number of nodes in this subtree
         boolean lr; // if true left/right comparison, else up/down
-        Node(Point2D p,boolean lr,int N) {
+        Node(Point2D p,boolean lr) {
             this.p=p;
-            this.N=N;
             this.lr=lr;
         }
     }
 
     // construct an empty set of points
     public KdTree() {
-        root=new Node(null,true,0);
-        root.ld=null;
-        root.ru=null;
+
     }
 
     // is the set empty?
@@ -32,46 +29,88 @@ public class KdTree {
         }
     }
 
-    private int size(Node x)
-    {
-        if (x == null) return 0;
-        else return x.N;
-    }
-
     private int size()
     {
-        if (root == null) return 0;
-        else return root.N;
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-        // if kdtree is empty, just insert the point as the root
-        if ( root.p == null ) {
-            root.p = p;
-            root.N=1;
-            return;
-        }
-
-        // if p is root, stop
-        if ( p == root.p ) {
-            return;
-        }
-        else
-        {
-            Node n=root;
-
-            if ( p.x <= n.x )
-
-        }
+        if (p == null) throw new IllegalArgumentException("calls insert() with a null point");
+        root = insert(root, p,false);
+        //assert check();
     }
 
-    // does the set contain point p?
+    private Node insert(Node x, Point2D p,boolean lr) {
+
+        if (x == null) {
+            size=size+1;
+            return new Node(p,! lr);
+        }
+
+        //int cmp = key.compareTo(x.key);
+        //if      (cmp < 0) x.left  = put(x.left,  key, val);
+        //else if (cmp > 0) x.right = put(x.right, key, val);
+        //else              x.val   = val;
+        //x.size = 1 + size(x.left) + size(x.right);
+        //return x;
+        if(( p.x() == x.p.x()) && (p.y() == x.p.y() ) ) return x;
+
+        if ( x.lr ) {
+            if(p.x() <= x.p.x() ) {
+                x.ld=insert(x.ld,p,! lr);
+            }
+            else {
+                x.ru=insert(x.ru,p,! lr);
+            }
+        }
+        else {
+            if(p.y() <= x.p.y() ) {
+                x.ld=insert(x.ld,p,! lr);
+            }
+            else {
+                x.ru=insert(x.ru,p,! lr);
+            }
+        }
+
+        return x;
+    }
+
+
+    // does the tree contain point p?
     public           boolean contains(Point2D p)    {
-        if( s.contains(p) ) { return true; }
+        return contains(root,p,false);
+
+    }
+
+    private boolean contains(Node x,Point2D p, boolean lr) {
+        if(p==null) throw new IllegalArgumentException("calls contains() with a null point");
+        if( x==null ) return false;
+        if(( p.x() == x.p.x()) && (p.y() == x.p.y() ) ) return true;
+
+        if ( x.lr ) {
+            if(p.x() <= x.p.x() ) {
+                contains(x.ld,p,! lr);
+            }
+            else {
+                contains(x.ru,p,! lr);
+            }
+        }
+        else {
+            if(p.y() <= x.p.y() ) {
+                contains(x.ld,p,! lr);
+            }
+            else {
+                contains(x.ru,p,! lr);
+            }
+        }
+
+
         return false;
     }
 
+
+    /*
     // draw all points to standard draw
     public              void draw()  {
         for(Point2D p: s ) {
@@ -105,9 +144,13 @@ public class KdTree {
         return nearest;
     }
 
+     */
+
     // unit testing of the methods (optional)
     public static void main(String[] args)   {
+        /*
         RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+
         StdDraw.enableDoubleBuffering();
         PointSET kdtree = new PointSET();
         while (true) {
@@ -126,6 +169,14 @@ public class KdTree {
             }
             StdDraw.pause(20);
         }
+
+         */
+
+        KdTree kd=new KdTree();
+        kd.insert(new Point2D(0.5,0.5));
+        StdOut.println("kd contains 0.5,0.5? "+kd.contains(new Point2D(0.5,0.5)));
+        kd.insert(new Point2D(0.4,0.4));
+        StdOut.println("kd contains 0.4,0.4? "+kd.contains(new Point2D(0.4,0.4)));
 
     }
 }
