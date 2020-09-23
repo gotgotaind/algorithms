3bf779f4f29892ac010af50716d1ddab2391767a
gotgotaind <gotgotaind@gmail.com>
2020-09-24 00:00:24 +0200

    Seems to work!

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -1,6 +1,10 @@
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -10,9 +14,12 @@ public class BaseballElimination {
     private final HashMap<String, Integer> team_i;
     private final int[] w, l, r;
     private final int[][] g;
+    private boolean[] isComputed;
+    private boolean[] isEliminated;
+    private Object[] certificateOfElimination;
 
-    public BaseballElimination(
-            String filename)                    // create a baseball division from given filename in format specified below
+    // create a baseball division from given filename in format specified below
+    public BaseballElimination(String filename)
     {
         In in = new In(filename);
 
@@ -24,11 +31,16 @@ public class BaseballElimination {
         l = new int[nt];
         r = new int[nt];
         g = new int[nt][nt];
+        isComputed = new boolean[nt];
+        isEliminated = new boolean[nt];
+        certificateOfElimination = new Object[nt];
+
 
         int i = 0;
         while (!in.isEmpty()) {
             teams[i] = in.readString();
             team_i.put(teams[i], i);
+            isComputed[i] = false;
             w[i] = in.readInt();
             l[i] = in.readInt();
             r[i] = in.readInt();
@@ -37,54 +49,173 @@ public class BaseballElimination {
             }
             i = i+1;
         }
+
+
+
+
+    }
+    // number of teams
+    public              int numberOfTeams()
+    {
+        return nt;
     }
 
-public              int numberOfTeams()                        // number of teams
-{
-    return nt;
-}
+    // all teams
+    public Iterable<String> teams()
+    {
+        return Arrays.asList(teams);
+    }
 
-public Iterable<String> teams()                                // all teams
-{
-    return Arrays.asList(teams);
-}
+    // number of wins for given team
+    public int wins(String team)
+    {
+        if (!team_i.containsKey(team)) throw new IllegalArgumentException();
+        return w[team_i.get(team)];
+    }
 
-public              int wins(String team)                      // number of wins for given team
-{
-    if (!team_i.containsKey(team)) throw new IllegalArgumentException();
-    return w[team_i.get(team)];
-}
+    // number of losses for given team
+    public int losses(String team)
+    {
+        if (!team_i.containsKey(team)) throw new IllegalArgumentException();
+        return l[team_i.get(team)];
+    }
 
-public              int losses(String team)                    // number of losses for given team
-{
-    if (!team_i.containsKey(team)) throw new IllegalArgumentException();
-    return l[team_i.get(team)];
-}
+    // number of remaining games for given team
+    public int remaining(String team)
+    {
+        if (!team_i.containsKey(team)) throw new IllegalArgumentException();
+        return r[team_i.get(team)];
+    }
 
-public              int remaining(String team)                 // number of remaining games for given team
-{
-    if (!team_i.containsKey(team)) throw new IllegalArgumentException();
-    return r[team_i.get(team)];
-}
+    // number of remaining games between team1 and team2
+    public int against(String team1, String team2)
+    {
+        if (!team_i.containsKey(team1) || !team_i.containsKey(team2)) throw new IllegalArgumentException();
+        return g[team_i.get(team1)][team_i.get(team2)];
+    }
 
-public              int against(String team1, String team2)    // number of remaining games between team1 and team2
-{
-    if (!team_i.containsKey(team1) || !team_i.containsKey(team2)) throw new IllegalArgumentException();
-    return g[team_i.get(team1)][team_i.get(team2)];
-}
+    private void compute(String team) {
+        int ti = team_i.get(team);
 
-/*
-public          boolean isEliminated(String team)              // is given team eliminated?
-public Iterable<String> certificateOfElimination(String team)  // subset R of teams that eliminates given team; null if not eliminated
- */
+        // trivial elimination ?
+        for (int i = 0; i < nt; i++) {
+            if (i != ti) {
+                if ((w[ti] + r[ti]) < w[i]) {
+                    isEliminated[ti] = true;
+                    certificateOfElimination[ti] = new ArrayList<String>();
+                    ((ArrayList<String>) certificateOfElimination[ti]).add(teams[i]);
+                    isComputed[ti] = true;
+                    return;
+                }
+            }
+        }
 
-public static void main(String[] args) {
-    BaseballElimination be = new BaseballElimination("teams4.txt");
-    StdOut.println(be.numberOfTeams());
-    StdOut.println(be.wins("Atlanta"));
-    StdOut.println(be.losses("New_York"));
-    StdOut.println(be.against("Philadlphia", "Montreal"));
-}
+
+        int n_games_vertices = (nt-1)*(nt-1) - (nt-1);
+
+
+
+        // 2 because there is the source and target vertices, nt-1 is the number of teams vertices
+        FlowNetwork G = new FlowNetwork(2 + n_games_vertices + (nt-1) );
+
+        // construct an array giving the id of the vertex for team i and connect it to the target
+        int[] team_vertex_id=new int[nt];
+        for (int i = 0; i < nt; i++) {
+            if (i < ti) {
+                team_vertex_id[i]=1+n_games_vertices+i;
+                FlowEdge e = new FlowEdge(team_vertex_id[i], G.V()-1, w[ti] + r[ti] - w[i]);
+                G.addEdge(e);
+            }
+            else if (i > ti)
+            {
+                team_vertex_id[i]=1+n_games_vertices+i-1;
+                FlowEdge e = new FlowEdge(team_vertex_id[i], G.V()-1, w[ti] + r[ti] - w[i]);
+                G.addEdge(e);
+            }
+        }
+
+        // connection the source vertex 0 to team i vs team j vertices ( excluding the team we're computing )
+        int v = 1;
+        for (int i = 0; i < nt; i++) {
+            for (int j = 0; j < nt; j++) {
+                if (i != ti && j != ti && i != j) {
+                    // source to game vertices
+                    FlowEdge e = new FlowEdge(0, v, g[i][j]);
+                    G.addEdge(e);
+                    // game to the teams vertices
+                    e = new FlowEdge(v, team_vertex_id[i], Double.POSITIVE_INFINITY);
+                    G.addEdge(e);
+                    e = new FlowEdge(v, team_vertex_id[j], Double.POSITIVE_INFINITY);
+                    G.addEdge(e);
+
+                    v = v + 1;
+                }
+            }
+        }
+
+        FordFulkerson ff = new FordFulkerson(G, 0, G.V() - 1);
+        for (FlowEdge e : G.adj(0)) {
+            if( e.flow() < e.capacity() ) {
+                isEliminated[ti] = true;
+                isComputed[ti] = true;
+                certificateOfElimination[ti] = new ArrayList<String>();
+                for (int i = 0; i < nt; i++) {
+                    if (i < ti) {
+                        if (ff.inCut(team_vertex_id[i])) {
+                            ((ArrayList<String>) certificateOfElimination[ti]).add(teams[i]);
+                        }
+                    }
+                    else if (i > ti)
+                    {
+                        if (ff.inCut(team_vertex_id[i])) {
+                            ((ArrayList<String>) certificateOfElimination[ti]).add(teams[i]);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    // is given team eliminated?
+    public boolean isEliminated(String team) {
+        if (!isComputed[team_i.get(team)]) {
+            compute(team);
+        }
+        return isEliminated[team_i.get(team)];
+    }
+
+    // subset R of teams that eliminates given team; null if not eliminated
+    public Iterable<String> certificateOfElimination(String team)  {
+        if (!isComputed[team_i.get(team)]) {
+            compute(team);
+        }
+        return ((ArrayList<String>) certificateOfElimination[team_i.get(team)]);
+    }
+
+
+    public static void main(String[] args) {
+        BaseballElimination be = new BaseballElimination("teams4.txt");
+        StdOut.println(be.numberOfTeams());
+        StdOut.println(be.wins("Atlanta"));
+        StdOut.println(be.losses("New_York"));
+        StdOut.println(be.against("Philadelphia", "Montreal"));
+
+        BaseballElimination division = new BaseballElimination("teams5.txt");
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
+
+    }
 
 }
 
