ab2774dbdddd7366c81d4460a16c00297c0c336e
gotgotaind <gotgotaind@gmail.com>
2021-01-21 22:55:59 +0100

    Using quick3string to sort the CircularSuffixArray I get : Correctness:  73/73 tests passed Memory:       10/10 tests passed Timing:       163/163 tests passed

--- burrows/src/CircularSuffixArray.java
+++ burrows/src/CircularSuffixArray.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.Quick3way;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.SuffixArray;
 import edu.princeton.cs.algs4.TrieSET;
 import edu.princeton.cs.algs4.TrieST;
 
@@ -14,32 +15,131 @@ public class CircularSuffixArray {
     private int l;
     private char[] ca;
     private int[] index;
+    private static final int CUTOFF =  5;   // cutoff to insertion sort (any value between 0 and 12)
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
-        if( s==null ) throw new IllegalArgumentException();
-        l=s.length();
+        String method = "3quick_fromsuffixarrayx";
+        //String method = "my_own";
+        if (s == null) throw new IllegalArgumentException();
+        l = s.length();
+        index = new int[l];
         // making a char array of the original string copied twice
         // so that we can take slice of it to make the circular
         // char arrays
-        this.ca=(s+s).toCharArray();
-        index=new int[l];
-        ics[] icsa=new ics[l];
-        // StdOut.println("icsa before sorting :");
-        for(Integer i=0; i<l; i++) {
-            icsa[i]=new ics(i);
-            // StdOut.println(icsa[i].icss());
+        this.ca = (s + s).toCharArray();
+
+        if (method == "my_own") {
+            // my method gets 100% completion on the grader but misses 3 performance points
+
+
+            ics[] icsa = new ics[l];
+            // StdOut.println("icsa before sorting :");
+            for (Integer i = 0; i < l; i++) {
+                icsa[i] = new ics(i);
+                // StdOut.println(icsa[i].icss());
+            }
+
+
+            Merge.sort(icsa);
+
+            // StdOut.println("icsa after sorting :");
+            for (Integer i = 0; i < l; i++) {
+                index[i] = icsa[i].ith;
+                // StdOut.println(icsa[i].icss());
+            }
         }
 
-
-        Arrays.sort(icsa);
-
-        // StdOut.println("icsa after sorting :");
-        for(Integer i=0; i<l; i++) {
-            index[i]=icsa[i].ith;
-            // StdOut.println(icsa[i].icss());
+        if (method == "SuffixArray") {
+            SuffixArray sa = new SuffixArray(s);
+            for (Integer i = 0; i < l; i++) {
+                index[i] = sa.index(i);
+                // StdOut.println(icsa[i].icss());
+            }
         }
+
+        if (method == "3quick_fromsuffixarrayx") {
+            // 3-way string quicksort lo..hi starting at dth character
+            for (int i = 0; i < l; i++)
+                index[i] = i;
+
+            sort(0, l-1, 0);
+
+            /*
+            // check the sort
+
+            for (int i = 0; i < l - 1; i++)
+                if( ! less(index[i],index[i+1],0 ) ) {
+                    StdOut.println(i);
+                    StdOut.println(ics_debug(index[i]));
+                    StdOut.println(ics_debug(index[i+1]));
+                    throw new IllegalStateException("");
+                }
+            */
+            }
+
+
     }
+
+        // 3-way string quicksort lo..hi starting at dth character ( fromsuffixarrayx )
+        private void sort(int lo, int hi, int d) {
+
+            // cutoff to insertion sort for small subarrays
+            if (hi <= lo + CUTOFF) {
+                insertion(lo, hi, d);
+                return;
+            }
+
+            int lt = lo, gt = hi;
+            char v = ca[index[lo] + d];
+            int i = lo + 1;
+            while (i <= gt) {
+                char t = ca[index[i] + d];
+                if      (t < v) exch(lt++, i++);
+                else if (t > v) exch(i, gt--);
+                else            i++;
+            }
+
+            // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
+            sort(lo, lt-1, d);
+            if (v >= 0  && d<l ) sort(lt, gt, d+1);
+            sort(gt+1, hi, d);
+        }
+
+        // sort from a[lo] to a[hi], starting at the dth character
+        private void insertion(int lo, int hi, int d) {
+            for (int i = lo; i <= hi; i++)
+                for (int j = i; j > lo && less(index[j], index[j-1], d); j--)
+                    exch(j, j-1);
+        }
+
+        // is text[i+d..n) < text[j+d..n) ?
+        private boolean less(int i, int j, int d) {
+            if (i == j) return false;
+            i = i + d;
+            j = j + d;
+            int k = 0;
+            // in suffixarrayx was this, because only the suffixes are compared
+            //while (i < l && j < l) {
+            // but wwe want to compare the whole circularsuffix so we may need to compare l characters
+            while (k < l && i < 2*l && j < 2*l ) {
+                if (ca[i] < ca[j]) return true;
+                if (ca[i] > ca[j]) return false;
+                i++;
+                j++;
+                k++;
+            }
+            // in suffixarrayx was this
+            // return i > j;
+            return false;
+        }
+
+        // exchange index[i] and index[j]
+        private void exch(int i, int j) {
+            int swap = index[i];
+            index[i] = index[j];
+            index[j] = swap;
+        }
 
     // ith circular suffix
     private class ics implements Comparable<ics> {
@@ -83,7 +183,7 @@ public class CircularSuffixArray {
         StringBuilder sb=new StringBuilder();
         int l=10000;
         for(int i=0; i<l; i++) {
-            sb.append((char) StdRandom.uniform(128));
+            sb.append((char) (StdRandom.uniform(25) ) );
         }
 
         Stopwatch timer = new Stopwatch();
@@ -91,13 +191,19 @@ public class CircularSuffixArray {
         // CircularSuffixArray c=new CircularSuffixArray(sb.toString());
         // abra.txt
         CircularSuffixArray c=new CircularSuffixArray("ABRACADABRA!");
+        // ABABBABABBABABB
+        //CircularSuffixArray c=new CircularSuffixArray("ABABBABABBABABB");
+        // stars.txt
+        // CircularSuffixArray c=new CircularSuffixArray("**");
+        // BAA
+        //CircularSuffixArray c=new CircularSuffixArray("BAA");
         // cadabra.txt
         // CircularSuffixArray c=new CircularSuffixArray("CADABRA!ABRA");
         //CircularSuffixArray c=new CircularSuffixArray("ABABABABABAB");
         //CircularSuffixArray c=new CircularSuffixArray("ABAB");
 
         for(int i=0; i<c.length(); i++ ) {
-            StdOut.println(c.ics_debug(c.index[i])+"\t"+c.index[i]);
+            StdOut.println(c.ics_debug(i)+"\t"+i+"\t"+c.ics_debug(c.index[i])+"\t"+c.index[i]);
         }
         StdOut.println("Finished in "+timer.elapsedTime());
 
